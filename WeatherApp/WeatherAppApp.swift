01d8523bb1b263468f297d27e0a1fa883171ca06
Kevashan Govender <23380129+KevashanGovender@users.noreply.github.com>
2024-07-13 23:15:57 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kevashan Govender on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
